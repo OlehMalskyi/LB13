5bc71be5c5033a2a4877ee37bf388317e4f790ce
OlehMalskyi <omalskij3@gmail.com>
2023-11-23 23:19:08 +0200

    lb13.1

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB13.1/sum.h"
+#include "../LB13.1/var.h"
+#include "../LB13.1/dod.h"
+#include "../LB13.1/sum.cpp"
+#include "../LB13.1/var.cpp"
+#include "../LB13.1/dod.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace nsSum;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestSumFunction)
+        {
+            nsVar::x = 1;
+            nsVar::e = 0.0001;
+
+            sum();
+
+            Assert::AreEqual(0.540303, nsVar::s, 0.001);
+        }
+    };
+}
